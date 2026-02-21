1dc3ed494daedc858cea3a3198bdf2064d33dc4a
Youssef0ssama <youssefossama210@gmail.com>
2026-02-21 17:51:22 +0200

    linq lab 1

---
+++ linQ labs/lab 1/lab 1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtID = new TextBox();
+            txtName = new TextBox();
+            txtDept = new TextBox();
+            ID = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            btnDisplay = new Button();
+            btnInsert = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnSearch = new Button();
+            dgvEmployees = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvEmployees).BeginInit();
+            SuspendLayout();
+            // 
+            // txtID
+            // 
+            txtID.Location = new Point(42, 9);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(125, 27);
+            txtID.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(256, 9);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(217, 27);
+            txtName.TabIndex = 1;
+            // 
+            // txtDept
+            // 
+            txtDept.Location = new Point(597, 9);
+            txtDept.Name = "txtDept";
+            txtDept.Size = new Size(125, 27);
+            txtDept.TabIndex = 2;
+            // 
+            // ID
+            // 
+            ID.AutoSize = true;
+            ID.Location = new Point(12, 12);
+            ID.Name = "ID";
+            ID.Size = new Size(24, 20);
+            ID.TabIndex = 3;
+            ID.Text = "ID";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(200, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(502, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Department";
+            // 
+            // btnDisplay
+            // 
+            btnDisplay.Location = new Point(12, 53);
+            btnDisplay.Name = "btnDisplay";
+            btnDisplay.Size = new Size(94, 29);
+            btnDisplay.TabIndex = 6;
+            btnDisplay.Text = "Display";
+            btnDisplay.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(155, 53);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(94, 29);
+            btnInsert.TabIndex = 7;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(327, 53);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 8;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(497, 53);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(683, 53);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(94, 29);
+            btnSearch.TabIndex = 10;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // dgvEmployees
+            // 
+            dgvEmployees.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEmployees.Location = new Point(12, 95);
+            dgvEmployees.Name = "dgvEmployees";
+            dgvEmployees.RowHeadersWidth = 51;
+            dgvEmployees.Size = new Size(765, 343);
+            dgvEmployees.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvEmployees);
+            Controls.Add(btnSearch);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnInsert);
+            Controls.Add(btnDisplay);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(ID);
+            Controls.Add(txtDept);
+            Controls.Add(txtName);
+            Controls.Add(txtID);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvEmployees).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtID;
+        private TextBox txtName;
+        private TextBox txtDept;
+        private Label ID;
+        private Label label1;
+        private Label label2;
+        private Button btnDisplay;
+        private Button btnInsert;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnSearch;
+        private DataGridView dgvEmployees;
+    }
+}
