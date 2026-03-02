c1e0fcf987bb9f1a2bc7d9cd1e383b58055af34f
Youssef0ssama <youssefossama210@gmail.com>
2026-03-02 17:16:15 +0200

    removed a project

--- MVC/labs project/ITI/ITI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ITI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
